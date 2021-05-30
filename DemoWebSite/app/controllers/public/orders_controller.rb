886cf91ea536d5a1cc956148e6fc6891782d5b34
rd-yamatakuuu <orgchem.techac@gmail.com>
2021-05-30 14:36:40 +0000

    complete-order

--- DemoWebSite/app/controllers/public/orders_controller.rb
+++ DemoWebSite/app/controllers/public/orders_controller.rb
@@ -44,7 +44,21 @@ class Public::OrdersController < ApplicationController
   end
 
   def create
-    
+    @order = Order.new(order_params)
+    @order.customer_id = current_customer.id
+    @order.save
+    @customer = current_customer
+    @cart_items = @customer.cart_items
+    @cart_items.each do |cart_item|
+     order_detail = OrderDetail.new
+     order_detail.item_id = cart_item.item_id
+     order_detail.order_id = @order.id
+     order_detail.price = cart_item.item.price
+     order_detail.amount = cart_item.amount
+     order_detail.save
+     cart_item.destroy
+    end
+    redirect_to home_thanks_path
   end
 
   def index
--- DemoWebSite/config/routes.rb
+++ DemoWebSite/config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   root :to => 'homes#top'
   get 'home/about' => 'homes#about'
+  get 'home/thanks' => 'homes#thanks'
 
   namespace :admin do
     resources :customers, only: [:index, :show, :edit, :update]
